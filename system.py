add7e0a4f6dd33cb665974ce99f3e894cbff4e1e
f4udor <mauromanconi99.mm@gmail.com>
2026-06-18 09:47:33 +0200

    create main.py

--- system.py
+++ system.py
@@ -6,7 +6,9 @@ class BookingSystem:
     #creato la classe BookingSystem, che inizializza una lista vuota per le prenotazioni
     def list_bookings(self):
         return self.bookings
-
-
+    #metodo per elencare tutte le prenotazioni
+    def add_booking(self, booking: Booking):
+        self.bookings.append(booking)
+    #metodo per aggiungere una prenotazione alla lista delle prenotazioni
     
-    
+
